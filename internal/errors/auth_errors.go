fd5b38c22ba78c63e3994c6435c23fab323c8d2f
Arnob Chowdhury <chowdhury.sami.al@gmail.com>
2025-03-13 15:13:52 +0600

    refactor(signup): use SendErrorResponse utility function in signout handler

--- internal/errors/auth_errors.go
+++ internal/errors/auth_errors.go
@@ -27,15 +27,25 @@ func (e *AuthError) Error() string {
 	return e.message
 }
 
+func (e *AuthError) SetErrMessage(message string) {
+	e.message = message
+}
+
 func (e *AuthError) LogError() string {
 	return fmt.Sprintf("AuthError - Code: %s, Message: %s, Status Code: %d", e.code, e.message, e.statusCode)
 }
 
 var (
-	ErrUnauthorized            = NewAuthError("UNAUTHORIZED", "Unauthorized", http.StatusUnauthorized)
-	ErrNoAuthorizationHeader   = NewAuthError("NO_AUTH_HEADER", "No Authorization header", http.StatusUnauthorized)
-	ErrInvalidAuthHeader       = NewAuthError("INVALID_AUTH_HEADER", "Invalid Authorization header", http.StatusUnauthorized)
-	ErrTokenExpired            = NewAuthError("TOKEN_EXPIRED", "Token expired", http.StatusUnauthorized)
-	ErrInvalidToken            = NewAuthError("INVALID_TOKEN", "Invalid token", http.StatusUnauthorized)
-	ErrUnexpectedSigningMethod = NewAuthError("UNEXPECTED_SIGNING_METHOD", "Unexpected signing method", http.StatusUnauthorized)
+	ErrUnauthorized             = NewAuthError("UNAUTHORIZED", "Unauthorized", http.StatusUnauthorized)
+	ErrNoAuthorizationHeader    = NewAuthError("NO_AUTH_HEADER", "No Authorization header", http.StatusUnauthorized)
+	ErrInvalidAuthHeader        = NewAuthError("INVALID_AUTH_HEADER", "Invalid Authorization header", http.StatusUnauthorized)
+	ErrTokenExpired             = NewAuthError("TOKEN_EXPIRED", "Token expired", http.StatusUnauthorized)
+	ErrInvalidToken             = NewAuthError("INVALID_TOKEN", "Invalid token", http.StatusUnauthorized)
+	ErrUnexpectedSigningMethod  = NewAuthError("UNEXPECTED_SIGNING_METHOD", "Unexpected signing method", http.StatusUnauthorized)
+	ErrMalformedRequest         = NewAuthError("BAD_REQUEST", "Malformed request", http.StatusBadRequest)
+	ErrInternalServerError      = NewAuthError("INTERNAL_SERVER_ERROR", "Internal server error", http.StatusInternalServerError)
+	ErrDBUniqueConstraintFailed = NewAuthError("DB_UNIQUE_CONSTRAINT_FAILED", "Internal server error", http.StatusInternalServerError)
+	ErrUniqueConstraintFailed   = NewAuthError("UNIQUE_CONSTRAINT_FAILED", "Unique constraint failed", http.StatusBadRequest)
+	ErrInvalidRequestBody       = NewAuthError("INVALID_REQUEST_BODY", "Invalid request body", http.StatusBadRequest)
+	ErrUserCreationFailed       = NewAuthError("USER_CREATION_FAILED", "User creation failed", http.StatusBadRequest)
 )
